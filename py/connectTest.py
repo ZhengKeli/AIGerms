9346f917757747754ddc351853b7e11bb4787297
ZhengKeli <zhengkeli2009@126.com>
2017-06-24 17:31:58 +0800

    实现了通过api文件封装好的方法来进行io

--- py/connectTest.py
+++ py/connectTest.py
@@ -1,10 +1,5 @@
-import struct
-
 import api
 
-readThing = bytes(api.rawStdin.read(4 * 3))
-readFloats = struct.unpack('!3f', readThing)
-api.rawStdout.write(struct.pack('!f', readFloats[0]))
-api.rawStdout.write(struct.pack('!f', readFloats[1]))
-api.rawStdout.write(struct.pack('!f', readFloats[2]))
+readFloats = api.read_float_list(3)
+api.write_float_list(readFloats)
 api.rawStdout.flush()
