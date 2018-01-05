88087fdfff516c01b870b1d85493ebadee8a2b7a
ZhengKeli <zhengkeli2009@126.com>
2018-01-05 11:53:20 +0700

    强迫症

--- nerve/src/slice.py
+++ nerve/src/slice.py
@@ -6,8 +6,8 @@ from nerve.GermsNerveCore import GermsNerveCore
 core = GermsNerveCore("../graph/core", "core")
 
 val_feel_nutrients = np.array([(0.5, 0.5) for x in np.arange(-1.0, 1.0, 0.01)])
-val_feel_germs = np.array([(0, 0) for x in np.arange(-1.0, 1.0, 0.01)])
-val_feel_walls = np.array([(0, 0) for x in np.arange(-1.0, 1.0, 0.01)])
+val_feel_germs = np.array([(0, 0) for _ in np.arange(-1.0, 1.0, 0.01)])
+val_feel_walls = np.array([(0, 0) for _ in np.arange(-1.0, 1.0, 0.01)])
 val_feel = np.concatenate([val_feel_nutrients, val_feel_germs, val_feel_walls], -1)
 
 val_act = np.array([(x, 0) for x in np.arange(-1.0, 1.0, 0.01)])
